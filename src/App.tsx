2865419382542954a8d1a970928789180ca5e67b
ruizdani301 <ruizdani301@gmail.com>
2024-04-28 18:47:39 -0500

    update apptsx and app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import ModelPage from './components/ModelPage';
+import "./App.css";
+import { Input, Button } from "reactstrap";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ModelPage from "./components/ModelPage";
 
 function Home() {
   return <h2>Welcome to our Machine Learning Landing Page</h2>;
 }
 
 function Info() {
-  return <h2>This page provides information about our project.</h2>;
+  return (
+    <div>
+      <div>
+        <Input className="talk" type="textarea" />
+      </div>
+    </div>
+  );
 }
 
 function App() {
   return (
     <Router>
       <div className="header">
-        <nav>
+        <nav className="nav">
           <Link to="/">Home</Link>
           <Link to="/use">Use</Link>
-          <Link to="/info">Info</Link>
+          <Link to="/info">let's talk</Link>
         </nav>
+        <div className="logo">Logo</div>
       </div>
       <div className="main">
         <Routes>
